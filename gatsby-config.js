a96bd756778785f0a5ecc9c49fb1c36caee792d5
vesone <vesonedean@gmail.com>
2018-08-04 19:47:41 -0400

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'First React App!',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hey people</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Watch the Video!</Link>
+  </div>
+)
+
+export default IndexPage
