8d74726784d330ce2b21037b7248343ccbe268c0
vesone <vesonedean@gmail.com>
2018-08-04 20:49:50 -0400

    Change watch button

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hey people</h1>
     <p>Welcome to my new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Watch the Video!</Link>
+    <Link to="/page-2/">Watch the Video</Link>
   </div>
 )
 
